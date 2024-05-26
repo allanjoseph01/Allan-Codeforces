c02ac58697d2155f34d3e4d2a277156a2a667249
allanjoseph01 <allanjoseph3005@gmail.com>
2024-05-26 23:39:48 +0530

    26 may

---
+++ Bazoka and Mocha's Array.cpp
@@ -0,0 +1,45 @@
+#include<bits/stdc++.h>
+using namespace std;
+string solve(int lst[],int s){
+	int a=lst[0];
+	int i=1;
+//	if(s==1){
+//		return "YES";
+//	}
+	while(i<s){
+		if(lst[i]>=lst[i-1]){
+			i+=1;
+		}else{
+			break;
+		}
+	}
+	if(lst[i]>a){
+		return "NO";
+	}
+	i+=1;
+	while(i<s){
+		if(lst[i]>=lst[i-1]){
+			i+=1;
+		}else{
+			return "NO";
+		}
+	}
+	if(lst[i-1]>=a){
+		return "NO";
+	}
+	return "YES";
+}
+
+int main(){
+	int t;
+	cin >> t;
+	for(int _=0;_<t;_++){
+		int n;
+		cin >> n;
+		int arr[n];
+		for(int i=0;i<n;i++){
+			cin >> arr[i];
+		}
+		cout << solve(arr,n) << endl;
+	}
+}
---
+++ static_cast.cpp
@@ -0,0 +1,7 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+	char s="1";
+	int a=static_cast<int>(s);
+	cout << a;
+}
